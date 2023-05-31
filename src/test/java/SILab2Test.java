ec07d6a8f53a745431feb9b87cc17075ed24e7ef
Toki <gorjan4ilijevski@gmail.com>
2023-05-31 20:51:32 +0200

    add tests and their description in readme

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,18 +1,53 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
-
-public class SILab2Test {
+class SILab2Test {
 
     @Test
-    public void test1() {
-        assertEquals(SILab2.function(2), "EVEN");
-        assertEquals(SILab2.function(3), "ODD");
+    void testFunction_EveryBranch() {
+        User user1 = new User("user1", "password", "user1@mail.com");
+        User user2 = new User("user2", "password", "user2@mail.com");
+        List<User> allUsers = List.of(user1, user2);
+
+        assertThrows(RuntimeException.class, () -> SILab2.function(null, allUsers));
+
+        User userWithNullPassword = new User("user3", null, "user3@mail.com");
+        assertThrows(RuntimeException.class, () -> SILab2.function(userWithNullPassword, allUsers));
+
+        User userWithNullEmail = new User("user4", "password", null);
+        assertThrows(RuntimeException.class, () -> SILab2.function(userWithNullEmail, allUsers));
+
+        User userWithNullUsername = new User(null, "password", "user5@mail.com");
+        assertTrue(SILab2.function(userWithNullUsername, allUsers));
+
+        User userWithInvalidEmail = new User("user6", "password", "user6mailcom");
+        assertFalse(SILab2.function(userWithInvalidEmail, allUsers));
+
+        User userWithSameEmailAndUsername = new User("user1", "password", "user1@mail.com");
+        assertFalse(SILab2.function(userWithSameEmailAndUsername, allUsers));
+
+        User userWithInvalidPassword = new User("user7", "user7", "user7@mail.com");
+        assertFalse(SILab2.function(userWithInvalidPassword, allUsers));
+
+        User userWithPasswordWithoutSpecialChars = new User("user8", "password12", "user8@mail.com");
+        assertFalse(SILab2.function(userWithPasswordWithoutSpecialChars, allUsers));
     }
 
     @Test
-    public void test2() {
-        assertEquals(SILab2.function(4), "EVEN");
-        assertEquals(SILab2.function(9), "ODD");
+    void testFunction_MultipleCondition() {
+        User user1 = new User("user1", "password", "user1@mail.com");
+        List<User> allUsers = List.of(user1);
+
+        assertThrows(RuntimeException.class, () -> SILab2.function(null, allUsers));
+
+        User userWithNullPassword = new User("user2", null, "user2@mail.com");
+        assertThrows(RuntimeException.class, () -> SILab2.function(userWithNullPassword, allUsers));
+
+        User userWithNullEmail = new User("user3", "password", null);
+        assertThrows(RuntimeException.class, () -> SILab2.function(userWithNullEmail, allUsers));
+
+        User validUser = new User("user4", "password", "user4@mail.com");
+        assertTrue(SILab2.function(validUser, allUsers));
     }
 }
